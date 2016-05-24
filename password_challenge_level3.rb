6e19358899fba97de771696049be9503a51bf775
Mark Nuiver <mark@vrijblikken.nl>
2016-05-24 11:18:43 +0200

    Update password_challenge_level3.rb

--- password_challenge_level3.rb
+++ password_challenge_level3.rb
@@ -34,18 +34,43 @@ end
 def passwordcheck_3(p)
 	wordlist = ["the", "be", "to", "of", "and", "in", "that", "have", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"]
 	word_check = false
+	pass_bolean = false
 	wordlist.each do |i|
 		word_check = true if p.match i
 		if word_check == true
 			puts "Password failed because it contains the word '#{i}'"
+			pass_bolean = false
 		end
-		
 		break if word_check == true
 	end
-	if word_check == false
-		puts "Password accepted"
+	if word_check != true
+		un = p.match /[a-z]/
+		up = p.match /[A-Z]/
+		nu = p.match /\d/
+		sp = p.match (/\W/)
+		un == nil || up == nil ? ( lt = nil ) : (lt = true)
+		if lt == nil &&  nu != nil && sp != nil
+			pass_bolean = true
+		elsif lt != nil && nu == nil && sp != nil
+			pass_bolean = true
+		elsif lt != nil && nu != nil && sp == nil
+			pass_bolean = true
+		elsif lt != nil &&  nu != nil && sp != nil
+			pass_bolean = true
+		else
+			pass_bolean = false
+		end
+		if pass_bolean == true
+			puts "Password accepted"
+		else
+			puts "Password failed because of the characters"
+		end
 	end
+	#puts pass_bolean
+	return pass_bolean
 end
 
-passwordcheck_3("viagladiola")
-passwordcheck_3("22thing11")
+#tests
+passwordcheck_3("Tthlad!iola")
+passwordcheck_3("22thing!11")
+passwordcheck_3("22thiag11")
